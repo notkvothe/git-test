cd5dbf5d7310705bc9bbba91119acedab48deaf8
Jeff Nasser <jeff.nasser89@gmail.com>
2025-02-18 10:11:28 -0800

    initial commit

---
+++ block07/script.js
@@ -0,0 +1,11 @@
+let message = "this is the combo";
+let num1 = 11 - 1
+let num2 = 2 * 20
+let num3 = 33 + 6
+
+
+alert (`You have received this message because you have been chosen to open an important vault. Here is the secret combination: ${num1}-${num2}-${num3}`)
+
+
+
+
